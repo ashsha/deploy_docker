960e66b7f7032c22efca24dbeebcd045340e0670
ashsha <ph@sf.local>
2016-05-21 09:11:49 +0300

    echoes

--- RemoveOne.sh
+++ RemoveOne.sh
@@ -1,8 +1,14 @@
 dckr_srv=10.1.1.101
 last_port=$(cat /etc/httpd/conf/httpd.conf | grep BalancerMember | awk 'BEGIN { FS=":" } /1/ { print $3 }' | grep -o [0-9]* | tail -1)
-echo last used container port: $last_port
+echo Hola!
+echo Last used container port: $last_port
+echo ''
+
 sed -i "/${dckr_srv}:${last_port}/d" /etc/httpd/conf/httpd.conf
-ssh root@${dckr_srv} "docker stop app${last_port} && docker rm app${last_port}"
-echo container app${next_port} stoped and deleted
-echo apache will be restarted now...
+
+ssh root@${dckr_srv} "docker stop app${last_port}_balancer && docker rm app${last_port}_balancer"
+
+echo Docker container app${next_port}_balancer was stoped and deleted.
+echo Apache will be restarted now...
 systemctl restart httpd
+echo All done.
